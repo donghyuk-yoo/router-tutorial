38bad05af23a41c251f46821d5d3d97beaa07b81
I an <ydh8485@gmail.com>
2021-04-04 22:52:51 +0900

    Link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
