5627a0dc5cab7a6297a8c8489af770543e3e32fb
I an <ydh8485@gmail.com>
2021-04-04 23:32:38 +0900

    URL parameter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -13,10 +14,17 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/deeperstill">ds 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">길동 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       {/* 다른 path에 같은 컴포넌트 보여주기 */}
       <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const data = {
+  deeperstill: {
+    name: "딥",
+    description: "리액트를 좋아하는 개발자",
+  },
+  gildong: {
+    name: "홍길동",
+    description: "고전 소설 주인공",
+  },
+};
+// URL파라미터, 라우트로 사용되는 컴포넌트에서 받아 오는 match라는 객체 안의 params 값을 참조
+// match 객체안에는 현재 컴포넌트가 어떤 경로 규칙에 의해 보이는지에 대한 정보가 들어 있다.
+function Profile({ match }) {
+  const { username } = match.params;
+  const profile = data[username];
+  if (!profile) {
+    return <div>존재하지 않는 사용자</div>;
+  }
+
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+}
+
+export default Profile;
