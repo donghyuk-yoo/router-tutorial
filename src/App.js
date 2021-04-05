2ad3a3dfbb11428c8e6af4d163d0300b014b01d6
I an <ydh8485@gmail.com>
2021-04-06 05:31:06 +0900

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
@@ -22,11 +22,24 @@ function App() {
           <Link to="/history">History 예제</Link>
         </li>
       </ul>
-      <Route path="/" component={Home} exact={true} />
-      {/* 다른 path에 같은 컴포넌트 보여주기 */}
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <hr />
+      {/* switch 일치하는 단 하나의 라우트만을 렌더링 시켜준다. */}
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        {/* 다른 path에 같은 컴포넌트 보여주기 */}
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        {/* path를 따로 정의하지 않으면 모든 상황에 렌더링됨 */}
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재 안함:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
