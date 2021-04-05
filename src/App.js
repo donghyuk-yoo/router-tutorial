d02c4fd75868755f55ad5e3120c491dc525c7be7
I an <ydh8485@gmail.com>
2021-04-06 02:37:44 +0900

    sub route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -15,16 +16,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/deeperstill">ds 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">길동 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       {/* 다른 path에 같은 컴포넌트 보여주기 */}
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
