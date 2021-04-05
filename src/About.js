d889fdbab04cef6387127322ebe34d149a74db74
I an <ydh8485@gmail.com>
2021-04-05 23:41:23 +0900

    URL query

--- src/About.js
+++ src/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import qs from "qs";
 
-function About() {
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 이 설정을 통해 문자열 맨 앞의 ? 생략
+  });
+  const showDetail = query.detail === "true";
+
   return (
     <div>
       <h1>소개</h1>
       <p>라우터 기초</p>
+      {showDetail && <p>detail 값을 true로 설정</p>}
     </div>
   );
 }
